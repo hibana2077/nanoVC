82abd72b439f738de0cd8a84a693e2c526fca4af
hibana2077 <hibana2077@gmail.com>
2025-01-20 01:34:41 +0800

    Add TTS dataset generation and audio processing functionality

---
+++ main/data/data_vis.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+#load data
+data = np.load("./tts_dataset.npz", allow_pickle=True)
+
+# NpzFile './tts_dataset.npz' with keys: inputs, ground_truths
+inputs = data["inputs"]
+ground_truths = data["ground_truths"]
+
+from pprint import pprint
+
+# Print the first 5 pairs
+for i in range(5):
+    print(f"Pair {i+1}")
+    print("Input 1")
+    pprint(inputs[i][0].shape)
+    print("Input 2")
+    pprint(inputs[i][1].shape)
+    print("Ground Truth")
+    pprint(ground_truths[i].shape)
+    print()
