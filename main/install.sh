af4554ab1fefd868489d838c7a959b7d91c81767
hibana2077 <hibana2077@gmail.com>
2025-02-08 05:08:33 +0000

    Update install script to include pip upgrade and add comment for git configuration

--- main/install.sh
+++ main/install.sh
@@ -1,4 +1,5 @@
 apt update
 apt install -y ffmpeg nvtop
+# git config --global user.email "hibana2077@gmail.com" && git config --global user.name "hibana2077"
 python -m pip install --upgrade pip
 pip install -r requirements.txt
